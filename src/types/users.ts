ed31450b8948f8613687a7cbc3488363b725a8d2
AndyVeversHill <andrew.vevers.hill@icloud.com>
2026-01-07 22:08:21 +0700

    create types folder for future typing needs

---
+++ src/types/users.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const userSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+
+  image: z.string().nullable().optional(),
+  email: z.string(),
+  emailVerified: z.boolean(),
+
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+export type User = z.infer<typeof userSchema>;
