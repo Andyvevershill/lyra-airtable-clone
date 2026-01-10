2cb2fbb53e729bc374c8080ba5f643d9435b44bf
AndyVeversHill <andrew.vevers.hill@icloud.com>
2026-01-10 23:11:43 +0700

    add col and add row

---
+++ src/components/buttons/add-column-button.tsx
@@ -0,0 +1,55 @@
+import { useLoadingStore } from "@/app/stores/use-loading-store";
+import { api } from "@/trpc/react";
+import { AiOutlinePlus } from "react-icons/ai";
+
+interface Props {
+  tableId: string;
+}
+
+export default function AddColumnButton({ tableId }: Props) {
+  const setIsLoading = useLoadingStore((state) => state.setIsLoading);
+
+  const utils = api.useUtils();
+
+  const addColumn = api.column.addColumn.useMutation({
+    onMutate: () => {
+      setIsLoading(true);
+    },
+    onSuccess: () => {
+      // Invalidate columns query to refetch
+      utils.column.getTableColumns.invalidate({ tableId });
+
+      // Invalidate rows query to get cells for new column
+      utils.row.getRowsInfinite.invalidate({ tableId });
+    },
+    onError: (error) => {
+      console.error("Failed to add column:", error);
+    },
+    onSettled: () => {
+      setIsLoading(false);
+    },
+  });
+
+  const handleAddColumn = () => {
+    // do we need to do anything in here with the rows??
+    // should we pass the
+    console.log("created new col");
+    addColumn.mutate({
+      tableId,
+      type: "number",
+      name: "test col!",
+    });
+  };
+
+  return (
+    <div className="sticky top-0 flex h-full items-center justify-center">
+      <button
+        className="pointer flex h-8 w-8 items-center justify-center"
+        title="Add column"
+        onClick={handleAddColumn}
+      >
+        <AiOutlinePlus size={16} className="text-gray-600" />
+      </button>
+    </div>
+  );
+}
---
+++ src/components/buttons/add-row-button.tsx
@@ -0,0 +1,51 @@
+import { useLoadingStore } from "@/app/stores/use-loading-store";
+import { api } from "@/trpc/react";
+import { AiOutlinePlus } from "react-icons/ai";
+
+interface Props {
+  tableId: string;
+}
+
+export default function AddRowButton({ tableId }: Props) {
+  const setIsLoading = useLoadingStore((state) => state.setIsLoading);
+
+  const utils = api.useUtils();
+
+  const addRow = api.row.addRow.useMutation({
+    onMutate: () => {
+      setIsLoading(true);
+    },
+    onSuccess: () => {
+      // Invalidate columns query to refetch
+      utils.column.getTableColumns.invalidate({ tableId });
+
+      // Invalidate rows query to get cells for new column
+      utils.row.getRowsInfinite.invalidate({ tableId });
+    },
+    onError: (error) => {
+      console.error("Failed to add column:", error);
+    },
+    onSettled: () => {
+      setIsLoading(false);
+    },
+  });
+
+  const handleAddRow = () => {
+    // do we need to do anything in here with the rows??
+    // should we pass the
+    console.log("created new row!");
+    addRow.mutate({
+      tableId,
+    });
+  };
+
+  return (
+    <button
+      className="pointer flex h-8 w-8 items-center justify-center"
+      title="Add column"
+      onClick={handleAddRow}
+    >
+      <AiOutlinePlus size={16} className="text-gray-600" />
+    </button>
+  );
+}
